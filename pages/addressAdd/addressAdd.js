93e7fd90ddafa4ec305b1bacaf8e9b61c4167dfa
yufawu <13763048459@163.com>
2021-03-30 13:50:47 +0800

    新增地址

--- pages/addressAdd/addressAdd.js
+++ pages/addressAdd/addressAdd.js
@@ -1,66 +1,87 @@
 // pages/addressAdd/addressAdd.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    onInput(event) {
+        console.log(event.detail, '输入内容时触发')
+    },
+    onChange(event) {
+        console.log(event.detail, '输入内容时触发-change')
+    },
+    onFocus(event) {
+        console.log(event.detail, '输入框聚焦时触发')
+    },
+    onCountry() {
+        console.log('选择国家')
+        wx.navigateTo({
+            url: '../countryList/countryList'
+        })
+    },
+    addressSave() { //保存地址
+        console.log('保存地址')
+        wx.navigateTo({
+            url: '../addressManage/addressManage'
+        })
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  }
+    }
 })
