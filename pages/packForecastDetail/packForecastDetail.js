63540049439230caa7a5dea3220275960720170a
yufawu <13763048459@163.com>
2021-04-01 15:56:14 +0800

    新增页面：“预报详情”

---
+++ pages/packForecastDetail/packForecastDetail.js
@@ -0,0 +1,83 @@
+// pages/packForecastDetail/packForecastDetail.js
+const app = getApp()
+const api = require('../../network/api');
+const http = require('../../network/http.js');
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        packDetail: null
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+        this.packForecastDetail()
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+    packForecastDetail() { //已入库订单
+        let that = this
+        console.log(app.globalData.packageId, 'id号')
+        let params = {
+            "id": app.globalData.packageId
+        }
+        http.get(api.PackForecastDetail, params).then((res) => {
+            console.log("请求结果", res.data.data)
+            that.setData({
+                packDetail: res.data.data
+            })
+
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
