d2da1793a41f0e480a2b6b8e3b75ad74a3e01e2c
yufawu <13763048459@163.com>
2021-04-01 17:21:01 +0800

    个人中心，登录，完成

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,10 +1,12 @@
 // pages/mine/mine.js
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+        userInfo: null, //个人信息
 
     },
 
@@ -19,6 +21,15 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function() {
+        if (wx.getStorageSync('openId')) { //判断用户是否登录
+            console.log('用户已经登录--我的页面')
+
+        } else {
+            console.log('用户没有登录--我的页面')
+            wx.navigateTo({
+                url: '../login/login'
+            })
+        }
 
     },
 
@@ -26,42 +37,83 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function() {
-
+        if (wx.getStorageSync('userInfo')) {
+            this.setData({
+                userInfo: wx.getStorageSync('userInfo')
+            })
+        }
     },
+    packNoInWare() { //未入库
+        console.log('未入库')
+        wx.navigateTo({
+            url: '../packNoInWare/packNoInWare'
+        })
+    },
+    packInWare() { //已入库
+        console.log('已入库')
+        wx.navigateTo({
+            url: '../packInWare/packInWare'
+        })
+    },
+    packAbnormal() { //异常-待处理
+        console.log('异常-待处理')
+        wx.navigateTo({
+            url: '../packAbnormal/packAbnormal'
+        })
+    },
+    inDevelop() { //该功能正在开发中
+        Toast("该功能正在开发中")
+    },
+
+
+
+
     myCode() { //我的二维码
         console.log('我的二维码')
-        wx.navigateTo({
-            url: '../myCode/myCode'
-        })
+        Toast("该功能正在开发中")
+            // wx.navigateTo({
+            //     url: '../myCode/myCode'
+            // })
     },
     myShareList() { //我的分享列表
         console.log('我的分享列表')
-        wx.navigateTo({
-            url: '../myShareList/myShareList'
-        })
+        Toast("该功能正在开发中")
+            // wx.navigateTo({
+            //     url: '../myShareList/myShareList'
+            // })
     },
     packForecastManage() { //包裹预报管理列表
         console.log('包裹预报管理列表')
-        wx.navigateTo({
-            url: '../packForecastManage/packForecastManage'
-        })
+        Toast("该功能正在开发中")
+            // wx.navigateTo({
+            //     url: '../packForecastManage/packForecastManage'
+            // })
     },
     problemCommon() { //常见问题
         console.log('常见问题')
-        wx.navigateTo({
-            url: '../problemCommon/problemCommon'
-        })
+        Toast("该功能正在开发中")
+            // wx.navigateTo({
+            //     url: '../problemCommon/problemCommon'
+            // })
     },
     suggestions() { //投诉建议
         console.log('投诉建议')
+        Toast("该功能正在开发中")
             // wx.navigateTo({
             //     url: '../suggestions/suggestions'
             // })
     },
     informationNotice() { //通知资讯
         console.log('通知资讯')
+        Toast("该功能正在开发中")
+            // wx.navigateTo({
+            //     url: '../informationNotice/informationNotice'
+            // })
+    },
+    packForecast() { //一键预报
+        console.log('一键预报')
         wx.navigateTo({
-            url: '../informationNotice/informationNotice'
+            url: '../packForecast/packForecast'
         })
     },
     addressManage() { //地址管理
@@ -72,21 +124,24 @@ Page({
     },
     bindPhone() { //更改手机
         console.log('更改手机')
+        Toast("该功能正在开发中")
             // wx.navigateTo({
             //     url: '../bindPhone/bindPhone'
             // })
     },
     bindMailbox() { //绑定邮箱
         console.log('绑定邮箱')
+        Toast("该功能正在开发中")
             // wx.navigateTo({
             //     url: '../bindMailbox/bindMailbox'
             // })
     },
     about() { //关于我们
         console.log('关于我们')
-        wx.navigateTo({
-            url: '../about/about'
-        })
+        Toast("该功能正在开发中")
+            // wx.navigateTo({
+            //     url: '../about/about'
+            // })
     },
 
     /**
