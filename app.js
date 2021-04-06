cff0fcf6af810ebb357255841318abbe6a8709b5
yufawu <13763048459@163.com>
2021-04-06 15:43:25 +0800

    细节修改：在app.js中增加全局变量，app.wxss中统一修改字体大小颜色。

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    onLaunch() {
+        // 展示本地存储能力
+        const logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+        // 登录
+        wx.login({
+            success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            }
+        })
+    },
+    globalData: {
+        userInfo: null, //用户的微信信息
+        userId: null, //用户id
+        countryMsg: null, //选择的国家信息
+        packageId: null, //预报包裹id,用于查询详情
+        addressId: null, //地址id，用于查询某条地址详情
+        promoterOpenId: null, //分享者的openId
+
+    }
 })
--- network/api.js
+++ network/api.js
@@ -1,10 +1,12 @@
 //请求地址
 // const ApiRootUrl = 'http://api-sys.xfx361.com/appMiniPro/v1'; // http
 // const ApiRootUrl = 'https://api-sys.xfx361.com/appMiniPro/v1'; //https--线上
-// const ApiRootUrl = 'http://192.168.2.237:8089/api/mini'; //本地测试
-const ApiRootUrl = 'https://zschinese.com/api/mini'; //测试服务器
+// const ApiRootUrl = 'https://api-sys.xfx361.com/setgoods/api/mini';
+const ApiRootUrl = 'http://192.168.2.237:8089/api/mini'; //本地测试
+// const ApiRootUrl = 'https://zschinese.com/api/mini'; //测试服务器
 
 module.exports = {
+    GetBanner: ApiRootUrl + '/config/carousels', //首页轮播图
     WechatLogin: ApiRootUrl + '/login', //注册登录
     GetExpress: ApiRootUrl + '/setgoods/getCouriers', //获取快递公司列表
     GetGoodsType: ApiRootUrl + '/setgoods/getGoodsType', //获取物品属性
@@ -14,13 +16,20 @@ module.exports = {
     Abnormal: ApiRootUrl + '/setgoods/anomalyGoods', //查询异常的订单
     AddReceiver: ApiRootUrl + '/Receiversend/addUserReceiversend', //添加常用收件人
     GetReceiver: ApiRootUrl + '/Receiversend/getUserReceiversend', // 查询常用收件人 
-    SearchContent: ApiRootUrl + '/search/content', //渠道详情
-    GetSgin: ApiRootUrl + '/getSgin', //获取二维码scene数据
-    Getwxacodeunlimit: ApiRootUrl + '/getwxacodeunlimit', //获取带scene的二维码
+    GetReceiverDetail: ApiRootUrl + '/Receiversend/getReceiversendById', // 查询收件人详细信息
+    EditReceiver: ApiRootUrl + '/Receiversend/editReceiversend', // 修改常用收件人 
+    DeleteReceiver: ApiRootUrl + '/Receiversend/delReceiversend', // 删除常用收件人 
+    PackForecastDetail: ApiRootUrl + '/setgoods/getPreAwbById', //包裹预报详情
+    EditPackForecast: ApiRootUrl + '/setgoods/editPreAwb', //修改包裹预报
+    DeletePackForecast: ApiRootUrl + '/setgoods/delPreAwb', //删除包裹预报
+    GetWarehouse: ApiRootUrl + '/config/getValueByKey', //获取仓库信息
+    GetWays: ApiRootUrl + '/config/getValueByKey', //获取路线信息
+    GetSecneValue: ApiRootUrl + '/getUserByOpenId', //获取二维码scene数据
+    GetQrCode: ApiRootUrl + '/qrCode', //获取带scene的二维码
     BindInviteCode: ApiRootUrl + '/bindInviteCode', //绑定邀请码
     GetCompanyPreface: ApiRootUrl + '/getCompanyPreface', //查看公司介绍
     GetContactaddre: ApiRootUrl + '/getContactaddre', //查看公司联系地址
-    FindPartake: ApiRootUrl + '/findPartake', //分享用户
+    GetMyShare: ApiRootUrl + '/getMyShareUser', //分享用户的列表
     SearchLogo: ApiRootUrl + '/logo', // logo图查询
     GetNews: ApiRootUrl + '/news', // 获取新闻列表 
     GetNewsData: ApiRootUrl + '/newsdata', // 获取新闻详情 
