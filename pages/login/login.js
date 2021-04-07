d7523f8f9490dab3ab3866fa239269ae95fad774
yufawu <13763048459@163.com>
2021-04-07 11:45:44 +0800

    解决登录时多次请求接口，修改wx.navigateBack()的位置

--- pages/login/login.js
+++ pages/login/login.js
@@ -58,13 +58,14 @@ Page({
             var that = this;
             // 获取到用户的信息了，打印到控制台上看下
             console.log('获取的用户信息', e.detail.userInfo);
-            this.weChatLogin()
             wx.setStorageSync('userInfo', e.detail.userInfo)
-                // wx.reLaunch({ //跳转页面
-                //         url: '../index/index'
-                //     })
-                // wx.navigateBack() //返回上一页
-                //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+            this.weChatLogin()
+
+            // wx.reLaunch({ //跳转页面
+            //         url: '../index/index'
+            //     })
+            // wx.navigateBack() //返回上一页
+            //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
             that.setData({
                 isHide: false
             });
@@ -102,11 +103,13 @@ Page({
                     console.log('用户登录', resMsg)
                     wx.setStorageSync('openId', resMsg.data.data.openId)
                     wx.setStorageSync('userId', resMsg.data.data.id)
+                    wx.setStorageSync('tel', resMsg.data.data.tel)
                     app.globalData.userId = resMsg.data.data.id
+                    wx.navigateBack() //返回上一页
                 })
             }
         });
-        wx.navigateBack() //返回上一页
+
     },
 
     /**
