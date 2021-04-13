7d38283c7474fd8f682cf8191f06cab19ca67278
yufawu <13763048459@163.com>
2021-04-13 16:42:57 +0800

    认领异常件--完成

--- pages/abnormalClaim/abnormalClaim.js
+++ pages/abnormalClaim/abnormalClaim.js
@@ -9,12 +9,28 @@ Page({
      * 页面的初始数据
      */
     data: {
-        searchValue: '2021041216345400329450203', //搜索关键词
+        searchValue: null, //搜索关键词
         showDetail: false, //是否显示详情
-        packDetail: null, //包裹详情
+        showErr: false, //显示匹配错误
+        claimDetail: null, //包裹详情
+        packDetail: {
+            "hawbNo": null,
+            "pcs": 1, //商品数量
+            "cwt": 1, //商品重量
+            "expressCompany": null,
+            "productName": null,
+            "svalue": null,
+            "remark": null,
+            "goodsType": null,
+            "brand": null, //品牌
+            "isUnpack": "0", //是否拆包
+        }, //认领信息
+        isUnpack: '0', //是否拆包，需要先转成字符串
         addressList: null, //收货地址列表
         showAddress: false, //收货地址选择面板
         addressSelected: null, //已选择的收货地址信息
+        goodsList: null, //物品属性列表,
+        showGoods: false, //物品属性选择面板
         remarkClaim: null, //认领备注
 
     },
@@ -22,9 +38,7 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
-
-    },
+    onLoad: function(options) {},
 
     /**
      * 生命周期函数--监听页面初次渲染完成
@@ -53,8 +67,71 @@ Page({
     //     })
 
     // },
-    remarkClaimChange(e) {
-        this.data.remarkClaim = e.detail
+    onRidioClick(e) { //是否拆包
+        let name = e.currentTarget.dataset.name
+        this.data.packDetail.isUnpack = name.toString()
+        console.log(name, e.currentTarget, '是否拆包', this.data.packDetail)
+        this.setData({
+            isUnpack: this.data.packDetail.isUnpack
+        })
+
+    },
+    hawbNoChange(e) { //快递单号
+        this.data.packDetail.hawbNo = e.detail
+    },
+    productNameChange(e) { //物品名称
+        this.data.packDetail.productName = e.detail
+    },
+    brandChange(e) { //物品品牌
+
+        this.data.packDetail.brand = e.detail
+    },
+    svalueChange(e) { //物品价值
+        this.data.packDetail.svalue = e.detail
+    },
+    goodsTypeChange(e) { //物品属性
+        this.data.packDetail.goodsType = e.detail
+    },
+    pcsChange(e) { //商品数量
+        this.data.packDetail.pcs = e.detail
+    },
+    cwtChange(e) { //商品重量
+        this.data.packDetail.cwt = e.detail
+    },
+    remarkChange(e) { //商品备注
+        this.data.packDetail.remark = e.detail
+    },
+    onGoodsClick(e) {
+        if (this.data.goodsList == null) {
+            this.getGoodsType() //获取属性列表
+
+        }
+
+        this.setData({
+            showGoods: true,
+        });
+    },
+    onGoodsClose() {
+        console.log('关闭货物属性选择')
+        this.setData({ showGoods: false });
+    },
+    onGoodsSelect(event) {
+        console.log('选择货物属性', event, event.detail)
+        this.data.packDetail.goodsType = event.detail.name
+        this.setData({
+            packDetail: this.data.packDetail
+        })
+    },
+    getGoodsType() { //获取物品属性公司列表
+        let that = this
+        http.post(api.GetGoodsType).then((res) => {
+            console.log(res.data.data, '物品属性列表', this.goodsList)
+            that.goodsList = res.data.data
+            that.setData({
+                goodsList: res.data.data
+            })
+
+        })
     },
     getReceiver() {
         let that = this
@@ -134,7 +211,7 @@ Page({
         http.get(api.PackAbnormalQuery, params).then((res) => {
             if (res.data.msg == '操作成功') {
                 that.setData({
-                    packDetail: res.data.preAwb,
+                    claimDetail: res.data.preAwb,
                 })
             } else {
                 wx.showToast({
@@ -153,17 +230,18 @@ Page({
             console.log(res.data, 'res')
             if (res.data.msg == '操作成功') {
                 that.setData({
-                    packDetail: res.data.preAwb,
-                    showDetail: true
+                    claimDetail: res.data.preAwb,
+                    showDetail: true,
+                    showErr: false,
                 })
                 wx.showToast({
                     title: '匹配成功',
                     icon: 'none'
                 })
             } else {
-                wx.showToast({
-                    title: '匹配失败',
-                    icon: 'none'
+                that.setData({
+                    showErr: true,
+                    showDetail: false,
                 })
             }
         })
@@ -172,16 +250,27 @@ Page({
     claim() { //认领
         if (this.data.addressSelected) {
             let params = {
-                "orderNo": this.data.packDetail.hawbNo,
-                "userId": wx.getStorageSync('userId'),
+
+                "hawbNo": this.data.claimDetail.hawbNo,
+                "pcs": this.data.packDetail.pcs,
+                "cwt": this.data.packDetail.cwt, //商品重量
+                "expressCompany": this.data.claimDetail.expressCompany,
+                "productName": this.data.packDetail.productName,
+                "svalue": this.data.packDetail.svalue,
+                "remark": this.data.packDetail.remark,
+                "goodsType": this.data.packDetail.goodsType,
+                "brand": this.data.packDetail.brand,
+                "isUnpack": this.data.packDetail.isUnpack,
                 "receiversendId": this.data.addressSelected.id,
-                "remark": this.data.remarkClaim
+                "costomerId": wx.getStorageSync('userId')
             }
             console.log('异常件参数', params)
-            http.get(api.PackAbnormalClaim, params).then((res) => {
+            http.post(api.PackAbnormalClaim, params).then((res) => {
                 console.log('认领异常件', res.data)
-                if (res.data.msg == '认领成功') {
-                    wx.navigateBack() //返回异常件列表页
+                if (res.data.msg == '认领成功！！') {
+                    wx.navigateTo({ //返回异常件列表页
+                        url: '../abnormalList/abnormalList'
+                    })
                 } else {
                     wx.showToast({
                         title: res.data.msg,
